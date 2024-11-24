1ed1f094ee55ed38379a2a1265f86257ea143123
Itumeleng Seema <itumelengseema@outlook.com>
2024-11-24 21:47:59 +0200

    added Projects  componet in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import Header from '@/components/common/Header';
 import HeroSection from '@/components/home/HeroSection';
 import OurCommitment from '@/components/home/OurCommitment';
+import OurProjects from '@/components/home/OurProjects';
 import OurServices from '@/components/home/OurServices';
 import OurValues from '@/components/home/OurValues';
-
 
 export default function Home() {
   return (
@@ -13,6 +13,7 @@ export default function Home() {
       <OurCommitment />
       <OurServices />
       <OurValues />
+      <OurProjects />
     </>
   );
 }
