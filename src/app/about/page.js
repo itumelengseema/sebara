211a07eba32fed89bd74aeec14c02b22e83df0e9
Itumeleng Seema <itumelengseema@outlook.com>
2024-11-30 21:38:42 +0200

    added history section

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,11 +4,13 @@ import Header from '@/components/common/Header';
 import React from 'react';
 import AboutHero from '@/components/about/AboutHeroSection';
 import Accreditations from '@/components/about/Accreditations';
+import History from '@/components/about/history';
 
 function page() {
   return (
     <>
       <AboutHero />
+      <History />
       <Accreditations />
     </>
   );
